a5f4ec14e31e986789c0f2fa401a17d61a73a7c2
bdogan <bdogan85@gmail.com>
2015-11-12 17:52:39 +0200

    CleanUp

--- _git_pull_all.sh
+++
@@ -1,4 +0,0 @@
-# @path: /Users/bdogan/Project/wwdnsserver
-# @repository: https://github.com/PlatinMarket/win_dns_server.git
-#
-cd /Users/bdogan/Project/wwdnsserver && git reset --hard && git pull
