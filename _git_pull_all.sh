7fae90b3529f3ba6c07c616bcf03c8ba44e18fe0
bdogan <bdogan85@gmail.com>
2015-11-12 17:51:56 +0200

    Updates

---
+++ _git_pull_all.sh
@@ -0,0 +1,4 @@
+# @path: /Users/bdogan/Project/wwdnsserver
+# @repository: https://github.com/PlatinMarket/win_dns_server.git
+#
+cd /Users/bdogan/Project/wwdnsserver && git reset --hard && git pull
